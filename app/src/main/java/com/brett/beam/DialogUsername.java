d5271a02af38cab381567c982c6980da59c959fe
Rony BIEN AIME <ronybienaime@gmail.com>
2018-04-16 16:43:06 -0400

    Initial commit

---
+++ app/src/main/java/com/brett/beam/DialogUsername.java
@@ -0,0 +1,8 @@
+package com.brett.beam;
+
+/**
+ * Created by Ermano on 4/15/2018.
+ */
+
+public class DialogUsername {
+}
---
+++ app/src/main/java/com/brett/beam/MessageNFC.java
@@ -0,0 +1,8 @@
+package com.brett.beam;
+
+/**
+ * Created by Ermano on 4/14/2018.
+ */
+
+public class MessageNFC {
+}
